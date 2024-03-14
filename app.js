dca44b01fc29201a3319edb0194331b3e28a4888
Zayed M Uddin <zayed.md.uddin273@gmail.com>
2024-03-14 15:56:05 -0400

    Try Catch Block

--- app.js
+++ app.js
@@ -18,20 +18,25 @@ const evaluateExpression = function () {
     display.value = result.toString();
 }
 
-for (let i = 0; i < btns.length; i++) {
-    const button = btns[i];
-    button.addEventListener("click", function () {
-        const val = button.innerText;
-
-        if (val == "AC") {
-            currentValue = "";
-            display.value = currentValue;
-        } else if (val == "=") {
-            evaluateExpression();
-        } else {
-            currentValue += val;
-            console.log(currentValue);
-            display.value = currentValue;
-        }
-    });
+try {
+    for (let i = 0; i < btns.length; i++) {
+        const button = btns[i];
+        button.addEventListener("click", function () {
+            const val = button.innerText;
+            if (val == "AC") {
+                currentValue = "";
+                display.value = currentValue;
+            } else if (val == "=") {
+                evaluateExpression();
+            } else {
+                currentValue += val;
+                console.log(currentValue);
+                display.value = currentValue;
+            }
+        });
+    }
+} catch (error) {
+    console.error(error);
+    currentValue = "ERROR";
+    display.value = currentValue;
 }
